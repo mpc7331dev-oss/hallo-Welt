3fab6735d62484f6f892fec2b42700a1dee2d2c7
Matthew <matthewpclarke@live.co.uk>
2026-07-06 16:22:57 +0100

    Refactored validation function

--- AC2.3_q1.py
+++ AC2.3_q1.py
@@ -1,1 +1,21 @@
-###AC2.3_Q1_a###
+n = input("Enter your name: ")
+print("Welcome", n)
+user_name = input("Enter your name: ")
+print("Welcome", user_name)
+name = input("Enter your name: ")
+print("Welcome", name)
+name = input("Enter your name: ")
+print("Welcome", name)
+def welcome_user():
+
+    name = input("Enter your name: ")
+    print("Welcome", name)
+
+welcome_user()
+welcome_user()
+if age >= 18:
+    can_vote = True
+else:
+    can_vote = False
+    
+can_vote = age >= 18
